fabf3e5bfed7796f38866ff13efeeb04c7ae0ca8
m-crown <30960941+m-crown@users.noreply.github.com>
2024-09-29 23:33:03 +0100

    Update format_cath_af_domains.py

--- format_cath_af_domains.py
+++ format_cath_af_domains.py
@@ -2,12 +2,14 @@
 import pandas as pd
 
 cath_af = pd.read_csv("/raid/MattC/repos/ProCogGraphData/analysis/notebooks/cath-v4_3_0.alphafold-v2.2022-11-22 (1).tsv", sep = "\t")
-cath_af["af_accession"] = cath_af["domain_ID"].str.extract("^af_([A-Za-z0-9]+)_")
+cath_af["af-accession"] = cath_af["domain_ID"].str.extract("^af_([A-Za-z0-9]+)_")
+#specific to the formatting for our alphafold downloads, will add AF- and -F1-model_4 to the accession - normally this owuldnt be done as users would provide the structures.
+cath_af["accession"] = "AF-" + cath_af["af-accession"] + "-F1-model_4"
+
 cath_af[["domain_start", "domain_end"]] = cath_af["domain_ID"].str.extract("^af_[A-Za-z0-9]+_(\d+)_(\d+)", expand = True)
 cath_af[["domain_start", "domain_end"]] = cath_af[["domain_start", "domain_end"]].astype("int")
 cath_af["domain_profile"] = cath_af["sfam_id"] + ":" + cath_af["domain_start"].astype("str") + "-" + cath_af["domain_end"].astype("str")
-cath_af = cath_af.sort_values(["af_accession", "domain_start"])
-cath_af_grouped = cath_af.groupby("af_accession").agg({"domain_profile": list}).reset_index()
+cath_af = cath_af.sort_values(["accession", "domain_start"])
+cath_af_grouped = cath_af.groupby("accession").agg({"domain_profile": list}).reset_index()
 cath_af_grouped["domain_profile"] = cath_af_grouped["domain_profile"].str.join(";")
-cath_af_grouped.to_csv("cath_alphafold_domains.tsv.gz", sep = "\t", compression = "gzip", index = False)#.head(20)
-
+cath_af_grouped.to_csv("/raid/MattC/repos/alpha_cognate/cath_alphafold_domains.tsv.gz", sep = "\t", compression = "gzip", index = False)#.head(20)
