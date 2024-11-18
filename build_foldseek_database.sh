fd5c862e23a85ff60d51e6681fdf81fce15ddbfd
m-crown <30960941+m-crown@users.noreply.github.com>
2024-11-18 16:02:40 +0000

    Update build_foldseek_database.sh

--- build_foldseek_database.sh
+++ build_foldseek_database.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir pcg_assemblies
+cd pcg_assemblies
 find /raid/MattC/repos/ProCogGraphData/structures/ -name '*_bio-h.cif.gz' -print0 | xargs -0 -I {} ln -s {} .
 foldseek createdb --chain-name-mode 1 pcg_assemblies/ pcgDB
 foldseek createindex pcgDB pcgDB_index
