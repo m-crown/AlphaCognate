ed97140938bf551543fb34cb1b015d4248d4ae37
m-crown <30960941+m-crown@users.noreply.github.com>
2025-03-28 08:27:08 +0100

    Update MolStarViewer.tsx

--- app/frontend/src/components/MolStarViewer.tsx
+++ app/frontend/src/components/MolStarViewer.tsx
@@ -11,12 +11,20 @@ declare global {
   }
 }
 
+interface MolstarViewerProps {
+  url: string;
+}
+
 //Adapted from the molstar docs (https://molstar.org/docs/plugin/instance/)
 //Added the plugin UI spec and set to 100% of div to enable integration in grid component
-const MolstarViewer = () => {
+const MolstarViewer = ({ url }: MolstarViewerProps) => {
   const parent = createRef<HTMLDivElement>();
 
   useEffect(() => {
+    if (!url) {
+      console.error("MolstarViewer requires a 'url'.");
+    }
+
     async function init() {
       const defaultSpec = DefaultPluginUISpec()
       const spec = {
@@ -25,7 +33,7 @@ const MolstarViewer = () => {
           initial: {
             isExpanded: false,
             showControls: false,
-            controlsDisplay: 'reactive',
+            // controlsDisplay: 'reactive',
           },
         },
       }
@@ -35,12 +43,14 @@ const MolstarViewer = () => {
         spec,
         render: renderReact18,
       });
-
+      //adapt this to use molspecviewer
+      //can eventually have some spec components in the json
       const data = await window.molstar.builders.data.download(
-        { url: "https://files.rcsb.org/download/3PTB.pdb" }, /* replace with your URL */
+        { url: "http://localhost:8080/AF-A0A0B4K6Q5-F1-model_4_transplants_filtered.cif" }, /* replace with your URL */
         { state: { isGhost: true } }
       );
-      const trajectory = await window.molstar.builders.structure.parseTrajectory(data, "pdb");
+
+      const trajectory = await window.molstar.builders.structure.parseTrajectory(data, "mmcif");
       await window.molstar.builders.structure.hierarchy.applyPreset(
         trajectory,
         "default"
@@ -52,14 +62,18 @@ const MolstarViewer = () => {
       window.molstar?.dispose();
       window.molstar = undefined;
     };
-  }, []);
-
-    return (
+  }, [url]); // Re-run effect only if `url` changes
+  if (!url) {
+    return <div>Missing URL</div>;
+  }
+  return (
       <div
         ref={parent}
         style={{
           width: '100%',
           height: '100%',
+          minWidth: '40vh',  
+          minHeight: '80vh',
           position: 'relative',
         }}
       />
