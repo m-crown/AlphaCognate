b6dd9fcee37803b90664912e59d3e9e8185735d6
m-crown <30960941+m-crown@users.noreply.github.com>
2025-03-28 08:27:13 +0100

    Update StructurePage.tsx

--- app/frontend/src/pages/StructurePage.tsx
+++ app/frontend/src/pages/StructurePage.tsx
@@ -6,10 +6,8 @@ import TransplantTable from "../components/TransplantTable";
 import { Grid, Loader, Text } from "@mantine/core";
 
 type TransplantApiResponse = {
-  data: Array<Transplant>;
-  meta: {
-    totalRowCount: number;
-  };
+  transplant_data: Array<Transplant>;
+  structure_data : Structure;
 };
 
 type Transplant = {
@@ -19,14 +17,22 @@ type Transplant = {
   success: boolean;
 };
 
+//this is used in multiple places - abstract away
+type Structure = {
+  name: string;
+  url: string;
+  transplanted: boolean;
+};
+
 function StructurePage() {
   const { id } = useParams(); // Extract the ID from the URL
   const [transplants, setTransplants] = useState<Transplant[]>([]);
+  const [structure, setStructure] = useState<Structure | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (id) {
-      const fetchData = async () => {
+      const fetchTransplantData = async () => {
         const url = new URL(
           '/transplants',
           process.env.NODE_ENV === 'production'
@@ -37,26 +43,28 @@ function StructurePage() {
         try {
           const response = await fetch(url.href);
           const json = (await response.json()) as TransplantApiResponse;
-          console.log(json.data);
-          setTransplants(json.data); // Update state with fetched data
+
+          setTransplants(json.transplant_data); // Update state with fetched data
+          setStructure(json.structure_data);
+          console.log(json.structure);
+          console.log(json.transplants);
           setLoading(false); // Set loading to false after data is fetched
         } catch (error) {
           console.error(error);
           setLoading(false); // Set loading to false even if an error occurs
         }
       };
-      fetchData();
+      fetchTransplantData();
     }
   }, [id]);
 
   return (
     <Grid>
       <Grid.Col span={6}>
-        <AutoComplete />
         {loading ? <Loader /> : <TransplantTable data={transplants} />}
       </Grid.Col>
       <Grid.Col span={6}>
-        <MolStarViewer pdbUrl={id || ""} />
+        <MolStarViewer url={structure?.url || ""} />
       </Grid.Col>
     </Grid>
   );
